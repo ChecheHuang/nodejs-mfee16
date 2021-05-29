a6741bd2feaa691722237ce1ddf13d28302c8a08
Carl <qwerfcxzas1@gmail.com>
2021-05-29 14:28:11 +0800

    add gitignore

--- crawler/app.js
+++ crawler/app.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210529&stockNo=2330&_=1622262713687')
   .then(function (response) {
     // handle success
-    console.log(response.data);
+    console.log(response.data.date);
+    console.log(response.data.title);
   })
   
