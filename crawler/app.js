3dff59564db71b98bf026f66bc389e435dbe8797
Carl <qwerfcxzas1@gmail.com>
2021-05-29 12:38:58 +0800

    axios modify

---
+++ crawler/app.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210529&stockNo=2330&_=1622262713687')
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+  })
+  
