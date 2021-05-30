bbebaa1cea86440591591896eb51a460e64df8fa
Carl <qwerfcxzas1@gmail.com>
2021-05-30 11:45:20 +0800

    modify

--- crawler/app.js
+++ crawler/app.js
@@ -1,8 +1,35 @@
-const axios = require('axios');
-axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210529&stockNo=2330&_=1622262713687')
-  .then(function (response) {
-    // handle success
-    console.log(response.data.date);
-    console.log(response.data.title);
-  })
-  
+// https://www.twse.com.tw/exchangeReport/STOCK_DAY
+// ?response=json
+// &date=20210523
+// &stockNo=2610
+
+// npm i axios
+// 引入 axios
+const axios = require("axios");
+
+// TODO: 從 stock.txt 讀股票代碼進來
+// filesystem
+// npm i fs ??? -> 不用
+const fs = require("fs");
+
+fs.readFile("stock.txt", "utf8", (err, data) => {
+  if (err) {
+    return console.error("讀檔錯誤", err);
+  }
+  console.log(`讀到的 stock code: ${data}`);
+
+  axios
+    .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: "json",
+        date: "20210523",
+        stockNo: data,
+      },
+    })
+    .then(function (response) {
+      if (response.data.stat === "OK") {
+        console.log(response.data.date);
+        console.log(response.data.title);
+      }
+    });
+});
