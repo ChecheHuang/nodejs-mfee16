2354cc1cb22a0397c2dd8bf00b665497672a019d
Carl <qwerfcxzas1@gmail.com>
2021-05-30 11:42:02 +0800

    xhr-promise

--- basic/callback-hell.js
+++ basic/callback-hell.js
@@ -21,13 +21,16 @@ doWork("刷牙", 2000, function (err, result) {
         console.log(result);
         doWork("寫功課", 5000, function (err, result) {
             if (err) {
-              console.error(err);
+              console.error(err);    
               return;
             }
             console.log(result);
           });
     });
 });
+
+
+
 
 //   doWork("吃早餐", 3000, function (err, result) {
 //     if (err) {
