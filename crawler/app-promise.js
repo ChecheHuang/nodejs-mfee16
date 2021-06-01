d33c0c8a71ffe5f524b38f1c623915782ad89d4d
Carl <qwerfcxzas1@gmail.com>
2021-06-01 23:17:17 +0800

    homework

--- crawler/app-promise.js
+++ crawler/app-promise.js
@@ -1,14 +1,11 @@
-// https://www.twse.com.tw/exchangeReport/STOCK_DAY
-// ?response=json
-// &date=20210523
-// &stockNo=2610
-
 const axios = require("axios");
 const fs = require("fs");
-const moment = require("moment"); 
+const moment = require("moment");
 
+// console.log(moment().format()); // 2021-05-30T13:45:06+08:00
+// console.log(moment().format("YYYYMMDD")); // 20210530
 
-let readfilePromise = function () {
+function readFilePromise() {
   return new Promise((resolve, reject) => {
     fs.readFile("stock.txt", "utf8", (err, data) => {
       if (err) {
@@ -16,45 +13,27 @@ let readfilePromise = function () {
       }
       resolve(data);
     });
-  })
+  });
 }
 
-readfilePromise()
-.then(function(stockcode){
-  console.log(stockcode);
-  return axios
-  .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-    params: {
-      response: "json",
-      date: moment().format("YYYYMMDD"),
-      stockNo: stockcode,
-    },
+readFilePromise()
+  .then((stockCode) => {
+    console.log("stockCode:", stockCode);
+
+    return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: "json",
+        date: moment().format("YYYYMMDD"),
+        stockNo: stockCode,
+      },
+    });
   })
-  .then(function (response) {
+  .then((response) => {
     if (response.data.stat === "OK") {
       console.log(response.data.date);
       console.log(response.data.title);
     }
+  })
+  .catch((err) => {
+    console.error(err);
   });
-})
-.catch(function(err){
-  console.log(err);
-})
-
-
-
-// axios
-//   .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-//     params: {
-//       response: "json",
-//       date: "20210523",
-//       stockNo: data,
-//     },
-//   })
-//   .then(function (response) {
-//     if (response.data.stat === "OK") {
-//       console.log(response.data.date);
-//       console.log(response.data.title);
-//     }
-//   });
-
