8dc80899726959a968fc109d94020322b5fa677f
Carl <qwerfcxzas1@gmail.com>
2021-06-05 22:48:45 +0800

    mysql insert

---
+++ crawler/mysql-insert.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+const fs = require("fs");
+const moment = require("moment");
+const mysql = require('mysql');
+const Promise = require("bluebird");
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'stock',
+});
+connection = Promise.promisifyAll(connection);
+const readFileBlue = Promise.promisify(fs.readFile);
+
+(async function () {
+    try {
+        await connection.connectAsync();
+        let stockCode = await readFileBlue("stock.txt", "utf-8");
+        console.log("stockCode:", stockCode);
+
+        let stock = await connection.queryAsync(`SELECT stock_id FROM stock WHERE stock_id = ${stockCode}`);
+        if (stock.length == 0) {
+            console.log("Start to query")
+            let response = await axios.get(
+                `https://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`
+            );
+
+            let answers = response.data.suggestions
+                .map((function (item) {
+                    return item.split("\t");
+                }))
+                .find(function (item) {
+                    return item[0] === stockCode;
+                })
+            console.log(answers)
+            //TODO:answers[0],answers[1]
+            if (answers.length > 1) {
+                console.log("Insert data")
+                connection.queryAsync(
+                    `INSERT INTO stock (stock_id,stock_name) VALUE ('${answers[0]}','${answers[1]}')`)
+            }
+        }
+
+    } catch (err) {
+        console.error(err);
+    } finally {
+        connection.end();
+    }
+})();
+
+
+
