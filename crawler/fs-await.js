007af5f4ba15fb26f0e820640239e530a016ab5b
Carl <qwerfcxzas1@gmail.com>
2021-06-05 11:00:59 +0800

    fs

---
+++ crawler/fs-await.js
@@ -0,0 +1,61 @@
+const axios = require("axios");
+const fs = require("fs/promises");
+const moment = require("moment");
+
+// console.log(moment().format()); // 2021-05-30T13:45:06+08:00
+// console.log(moment().format("YYYYMMDD")); // 20210530
+
+// function readFilePromise() {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile("stock.txt", "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(data);
+//     });
+//   });
+// }
+async function crawler(){
+    try{
+        let stockCode = await fs.readFile("stock.txt", "utf8");
+        let response = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+        params: {
+          response: "json",
+          date: moment().format("YYYYMMDD"),
+          stockNo: stockCode,
+        },
+      });
+      if (response.data.stat === "OK") {
+        console.log(response.data.date);
+        console.log(response.data.title);
+      }
+    }catch(err){
+        console.error(err);
+    }finally{
+        console.log("全部跑完了")
+    }
+}
+crawler();
+
+
+// readFilePromise()
+//   .then((stockCode) => {
+//     console.log("stockCode:", stockCode);
+
+//     return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+//       params: {
+//         response: "json",
+//         date: moment().format("YYYYMMDD"),
+//         stockNo: stockCode,
+//       },
+//     });
+//   })
+//   .then((response) => {
+//     if (response.data.stat === "OK") {
+//       console.log(response.data.date);
+//       console.log(response.data.title);
+//     }
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
