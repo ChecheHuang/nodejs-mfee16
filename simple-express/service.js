8720705d9cb2b3d2f67816c716e625309838d4e1
Carl <qwerfcxzas1@gmail.com>
2021-06-19 20:16:53 +0800

    views

--- simple-express/service.js
+++ simple-express/service.js
@@ -1,8 +1,12 @@
+const connection = require("./utils/db.js")
+
 const express = require("express");
 let app = express();
 
 app.use(express.static("public"));
 
+app.set("views","views");
+app.set("view engine","pug");
 //middleware 中間件 中介函式
 app.use(function(req, res, next){
     let current = new Date;
@@ -12,17 +16,28 @@ app.use(function(req, res, next){
 });
 
 app.get("/", function(req, res){
-    res.send("Hello Express");
+    // res.send("Hello Express");
+    res.render("index");
 })
 app.get("/about", function(req, res){
-    res.send("About Express");
+    // res.send("About Express");
+    res.render("about");
 })
 app.get("/test", function(req, res){
     res.send("Test Express");
 })
 
+app.get("/stock",async(req, res)=>{
+    let queryResults = await connection.queryAsync("SELECT * FROM stock;")
+    
+    res.render("stock/list",{
+        stocks:queryResults
+    })
+});
 
 
-app.listen(3001, () => {
+app.listen(3001, async() => {
+    
+    await connection.connectAsync();
     console.log(`我跑起來了喔 在 port 3001`)
 })
